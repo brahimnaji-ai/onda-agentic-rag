27b38205f2366d6d2a824e291c1530ea9f7bfeed
Brahim Naji <ba3105947@gmail.com>
2026-08-16 12:20:57 +0100

    feat(persistence): add JPA auditing base entity and application config

---
+++ src/main/java/ma/onda/rag/shared/persistance/JpaAuditingConfig.java
@@ -0,0 +1,32 @@
+package ma.onda.rag.shared.persistance;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaAuditingConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (
+                authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken
+
+            ) {
+                return Optional.of("SYSTEM");
+            }
+            return Optional.ofNullable(authentication.getName());
+        };
+    }
+}
