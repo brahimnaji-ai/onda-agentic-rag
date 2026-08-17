4334eecb4b59f597d9cbefd370e30db684d446f5
Brahim Naji <ba3105947@gmail.com>
2026-08-17 22:30:31 +0100

    test(context): let hibernate create schema in full-context test

--- src/test/java/ma/onda/rag/OndaAgenticRagApplicationTests.java
+++ src/test/java/ma/onda/rag/OndaAgenticRagApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(properties = "spring.jpa.hibernate.ddl-auto=create-drop")
 class OndaAgenticRagApplicationTests {
 
     @Test
